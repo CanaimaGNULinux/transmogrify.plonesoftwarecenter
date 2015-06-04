9d4dcf5bdc8d3d5fc918cd64f133d7d5b6dc4ee9
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2015-06-04 11:58:31 -0430

    Added initial package struture

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='transmogrify.plonesoftwarecenter',
+      version=version,
+      description="A transmogrifier pipeline to convert PloneSoftwareCenter's items into default Plone content types.",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone transmogrifier pipeline PloneSoftwareCenter',
+      author='Leonardo J. Caballero G.',
+      author_email='leonardocaballero@gmail.com',
+      url='https://github.com/CanaimaGNULinux/transmogrify.plonesoftwarecenter',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['transmogrify'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
